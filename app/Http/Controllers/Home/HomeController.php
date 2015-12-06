a1d703eb1cff38beb2ba5c697248d9b8a46cb648
bibennurbani <bibennurbani@gmail.com>
2015-12-06 16:46:15 +0000

    coba nambah

---
+++ app/Http/Controllers/Home/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Home;
+
+use App\User;
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    /**
+     * Show the profile for the given user.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function infophp()
+    {
+        // Show all information, defaults to INFO_ALL
+        phpinfo();
+    }
+    
+    public function aboutme()
+    {
+        return view("aboutme");
+    }
+}
